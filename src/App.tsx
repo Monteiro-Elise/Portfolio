2f6a4ed5ec9125026b077f8a55d5fa15585dcca1
Monteiro-Elise <elise.ma.monteiro@gmail.com>
2026-03-08 12:09:29 +0100

    Setup Tailwind CSS with Light/Dark Theme (#14)
    - Added Tailwind CSS and @tailwindcss/vite plugin
    - Configured Tailwind and custom dark variant in index.css
    - Implemented useDarkMode hook with localStorage persistence and system preference fallback
    - Added ToggleDarkMode component and example usage in App.tsx
    - Updated Vite config to include Tailwind plugin

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import LanguageSwitcher from './components/LanguageSwitcher';
 import { useTranslation } from 'react-i18next';
+import ToggleDarkMode from './components/ToggleDarkMode';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,25 +12,28 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-white dark:bg-black">
+        <ToggleDarkMode />
+        <div>
+          <a href="https://vite.dev" target="_blank" rel="noreferrer">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank" rel="noreferrer">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <LanguageSwitcher />
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            {t('count')} {count}
+          </button>
+          <p>
+            {t('edit')} <code>src/App.tsx</code> {t('save')}
+          </p>
+        </div>
+        <p className="read-the-docs">{t('clic-logos')}</p>
       </div>
-      <h1>Vite + React</h1>
-      <LanguageSwitcher />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {t('count')} {count}
-        </button>
-        <p>
-          {t('edit')} <code>src/App.tsx</code> {t('save')}
-        </p>
-      </div>
-      <p className="read-the-docs">{t('clic-logos')}</p>
     </>
   );
 }

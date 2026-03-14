f577a9bf0f4df3616e0eba85ad5d3ee78c8a9ad5
Elise Monteiro <elise.ma.monteiro@gmail.com>
2026-03-14 14:21:16 +0100

    feat: Add base header layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Banner from './components/Banner';
 import Footer from './layouts/Footer';
+import Header from './layouts/Header';
 
 function App() {
   return (
     <div className="min-h-screen bg-primary transition-colors duration-300">
+      <Header />
       <main className="pt-9">
         <Banner />
       </main>
---
+++ src/layouts/Header.tsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-component backdrop-blur-sm"></header>
+  );
+}
+
+export default Header;

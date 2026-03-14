7fa5faa255e20f300585f43ff6b44cf8ffdf4d76
Elise Monteiro <elise.ma.monteiro@gmail.com>
2026-03-14 13:25:36 +0100

    chore: Update i18n configuration to disable old catalogs

--- i18next-parser.config.cjs
+++ i18next-parser.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   locales: ['en', 'fr'],
   output: 'src/locales/$LOCALE/$NAMESPACE.json',
+  createOldCatalogs: false,
   sort: true,
   keepRemoved: false,
   defaultNamespace: 'translation',

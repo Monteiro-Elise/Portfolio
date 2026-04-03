9dafcd5720f94a3c0a815a02e916c7176a0f37f7
Elise Monteiro <elise.ma.monteiro@gmail.com>
2026-04-03 13:23:44 +0200

    feat: Add header navigation to scroll between sections

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -9,43 +9,65 @@ import { useLanguage } from '../hooks/useLanguage';
 function Header() {
   const { t, currentLanguage } = useLanguage();
   const cvHref = `/cv/CV_Elise_MONTEIRO_${currentLanguage}.pdf`;
+  const scrollToSection = (sectionId: string) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-component backdrop-blur-sm">
-      {/* Actions */}
-      <div className="flex items-center justify-end gap-3 px-3">
-        {/* Download CV Button */}
-        <a
-          href={cvHref}
-          download={`${CONSTANTS.name} ${t(`resume`)}.pdf`}
-          className="icon-btn"
-        >
-          <FiDownload className="icon-header" />
-        </a>
+      <div className="flex justify-between">
+        {/* Navigation */}
+        <div className="flex items-center justify-center gap-3 px-10">
+          <nav className="hidden md:flex items-center gap-6">
+            {CONSTANTS.sections.map((section) => (
+              <button
+                key={section.id}
+                onClick={() => scrollToSection(section.id)}
+                className="icon-btn"
+              >
+                {t(`sections.${section.id}`)}
+              </button>
+            ))}
+          </nav>
+        </div>
+        {/* Actions */}
+        <div className="flex items-center justify-end gap-3 px-3">
+          {/* Download CV Button */}
+          <a
+            href={cvHref}
+            download={`${CONSTANTS.name} ${t(`resume`)}.pdf`}
+            className="icon-btn"
+          >
+            <FiDownload className="icon-header" />
+          </a>
 
-        {/* Github */}
-        <a
-          href={CONSTANTS.github}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="icon-btn"
-        >
-          <SiGithub className="icon-header" />
-        </a>
+          {/* Github */}
+          <a
+            href={CONSTANTS.github}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon-btn"
+          >
+            <SiGithub className="icon-header" />
+          </a>
 
-        {/* Linkedin */}
-        <a href={CONSTANTS.linkedin} className="icon-btn">
-          <FaLinkedin className="icon-header" />
-        </a>
+          {/* Linkedin */}
+          <a href={CONSTANTS.linkedin} className="icon-btn">
+            <FaLinkedin className="icon-header" />
+          </a>
 
-        {/* Mail */}
-        <a href={`mailto:${CONSTANTS.mail}`} className="icon-btn">
-          <FiMail className="icon-header" />
-        </a>
+          {/* Mail */}
+          <a href={`mailto:${CONSTANTS.mail}`} className="icon-btn">
+            <FiMail className="icon-header" />
+          </a>
 
-        <LanguageSwitcher />
+          <LanguageSwitcher />
 
-        <ToggleDarkMode />
+          <ToggleDarkMode />
+        </div>
       </div>
     </header>
   );

565ae950215fcf91f493f4c42890f4c13a110d53
Elise Monteiro <elise.ma.monteiro@gmail.com>
2026-04-03 14:03:07 +0200

    feat: Add name, job, phone and email to banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,34 @@
+import { CONSTANTS } from '../utils/constants';
+import { FiPhone, FiMail } from 'react-icons/fi';
+import { useLanguage } from '../hooks/useLanguage';
+
 function Banner() {
-  return <div className="h-48  bg-banner bg-cover bg-center"></div>;
+  const { t, currentLanguage } = useLanguage();
+  return (
+    <div className="h-48 bg-banner bg-cover bg-center">
+      <div className="relative z-10 container mx-auto px-4">
+        <div className="flex justify-end">
+          <div className="max-w-lg text-right">
+            <p className="text-2xl md:text-6xl text-white mb-2">
+              {CONSTANTS.name}
+            </p>
+            <p className="text-xl md:text-3xl mb-6 text-other">{t('job')}</p>
+            <div className="flex items-center justify-end gap-2 text-white">
+              <FiPhone className="icon-decoration" />
+              <span className="text-sm md:text-base">
+                {currentLanguage === 'fr'
+                  ? CONSTANTS.phoneLocal
+                  : CONSTANTS.phoneInternational}
+              </span>
+            </div>
+            <div className="flex items-center justify-end gap-2 text-white">
+              <FiMail className="icon-decoration" />
+              <span className="text-sm md:text-base">{CONSTANTS.mail}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Banner;

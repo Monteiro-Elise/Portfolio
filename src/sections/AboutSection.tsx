acf4d23d5bea8b21a6395fb04aaed5b42147c373
Elise Monteiro <elise.ma.monteiro@gmail.com>
2026-04-04 13:07:42 +0200

    feat: Add content to about section

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -1,8 +1,19 @@
+import { useLanguage } from '../hooks/useLanguage';
+
 function AboutSection() {
+  const { t } = useLanguage();
   return (
     <div className="section-content">
       <div className="text-lg leading-relaxed">
-        <p></p>
+        <p>{t('about.construction')}</p>
+        <br />
+        <p>
+          {t('about.intro')} {t('about.passion')}
+        </p>
+        <br />
+        <p>{t('about.skills')}</p>
+        <br />
+        <p>{t('about.contact')}</p>
       </div>
     </div>
   );

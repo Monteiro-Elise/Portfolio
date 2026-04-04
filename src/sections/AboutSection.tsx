af372e61e264259a93144554101e7fd090a27bc1
Monteiro-Elise <elise.ma.monteiro@gmail.com>
2026-04-04 13:22:03 +0200

    About Section (#33)
    # About Section
    - implement about section ui
    - add content to about section

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -1,5 +1,22 @@
+import { useLanguage } from '../hooks/useLanguage';
+
 function AboutSection() {
-  return <>Section 1</>;
+  const { t } = useLanguage();
+  return (
+    <div className="section-content">
+      <div className="text-lg leading-relaxed">
+        <p>{t('about.construction')}</p>
+        <br />
+        <p>
+          {t('about.intro')} {t('about.passion')}
+        </p>
+        <br />
+        <p>{t('about.skills')}</p>
+        <br />
+        <p>{t('about.contact')}</p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutSection;

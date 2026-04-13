81a7299c76d08ed2dbcef4789b5641fe095e7192
Elise Monteiro <elise.ma.monteiro@gmail.com>
2026-04-13 10:41:25 +0200

    chore: Remove unused code in useLanguage

--- src/hooks/useLanguage.tsx
+++ src/hooks/useLanguage.tsx
@@ -6,13 +6,11 @@ export function useLanguage() {
   const { i18n, t } = useTranslation();
 
   const currentLanguage = i18n.language;
-  const STORAGE_KEY = 'language';
 
   const changeLanguage = useCallback(
     (lang: string) => {
       if (lang === i18n.language) return;
       i18n.changeLanguage(lang);
-      localStorage.setItem(STORAGE_KEY, lang);
     },
     [i18n]
   );
@@ -20,14 +18,6 @@ export function useLanguage() {
   useEffect(() => {
     document.documentElement.lang = i18n.language;
   }, [i18n.language]);
-
-  useEffect(() => {
-    const savedLang = localStorage.getItem(STORAGE_KEY);
-
-    if (savedLang && savedLang !== i18n.language) {
-      i18n.changeLanguage(savedLang);
-    }
-  }, [i18n]);
 
   return {
     t,

b82a8f7f91cd8d8ac0c23f57616aaeaa66210211
Elise Monteiro <elise.ma.monteiro@gmail.com>
2026-04-20 21:22:03 +0200

    feat: Implement dynamic metadata, sitemap and robots.txt with vite-plugin-sitemap

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,57 @@ import Banner from './components/Banner';
 import Footer from './layouts/Footer';
 import Header from './layouts/header/Header';
 import SectionLayout from './layouts/SectionLayout';
-import { useEffect } from 'react';
 import { useLanguage } from './hooks/useLanguage';
 import { CONSTANTS } from './utils/constants';
+import { Helmet } from 'react-helmet-async';
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   const { t, currentLanguage } = useLanguage();
-  useEffect(() => {
-    document.title = `${t('title', {
-      name: `${CONSTANTS.name}`,
-    })}`;
-  }, [currentLanguage, t]);
 
   return (
-    <div className="min-h-screen bg-primary transition-colors duration-300">
-      <Header />
-      <main>
-        <Banner />
-        {CONSTANTS.sections.map((section, index) => {
-          const Component = section.component;
-          return (
-            <SectionLayout
-              key={index}
-              id={section.id}
-              index={index}
-              total={CONSTANTS.sections.length}
-            >
-              <Component />
-            </SectionLayout>
-          );
-        })}
-      </main>
-      <Footer />
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <html lang={currentLanguage} />
+
+        <title>{t('title', { name: CONSTANTS.name })}</title>
+
+        <meta name="description" content={t('description')} />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta name="author" content={CONSTANTS.name} />
+
+        <meta
+          property="og:title"
+          content={t('title', { name: CONSTANTS.name })}
+        />
+        <meta property="og:description" content={t('description')} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/preview.jpg" />
+        <meta property="og:url" content={CONSTANTS.domain} />
+      </Helmet>
+      <div className="min-h-screen bg-primary transition-colors duration-300">
+        <Header />
+        <main>
+          <Banner />
+          {CONSTANTS.sections.map((section, index) => {
+            const Component = section.component;
+            return (
+              <SectionLayout
+                key={index}
+                id={section.id}
+                index={index}
+                total={CONSTANTS.sections.length}
+              >
+                <Component />
+              </SectionLayout>
+            );
+          })}
+        </main>
+        <Footer />
+      </div>
+    </HelmetProvider>
   );
 }
 

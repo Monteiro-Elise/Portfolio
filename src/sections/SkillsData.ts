a141c1d2d2cb5e09cb6b0f5e647e79b81b33aa02
Elise Monteiro <elise.ma.monteiro@gmail.com>
2026-05-02 16:08:26 +0200

    perf: Improve skills section performance

---
+++ src/sections/SkillsData.ts
@@ -0,0 +1,82 @@
+import {
+  FaGitAlt,
+  FaPython,
+  FaDocker,
+  FaJava,
+  FaHtml5,
+  FaCss3Alt,
+  FaReact,
+  FaAngular,
+  FaVuejs,
+  FaNodeJs,
+  FaDatabase,
+} from 'react-icons/fa';
+import { FaC } from 'react-icons/fa6';
+import { FaGolang } from 'react-icons/fa6';
+import {
+  SiGithubactions,
+  SiPhp,
+  SiIntellijidea,
+  SiKotlin,
+  SiTypescript,
+  SiDjango,
+  SiSpring,
+  SiMongodb,
+} from 'react-icons/si';
+import { BiLogoVisualStudio } from 'react-icons/bi';
+import { DiEclipse, DiJavascript1, DiRedis } from 'react-icons/di';
+import { GrMysql } from 'react-icons/gr';
+import { BiLogoPostgresql } from 'react-icons/bi';
+
+export function getSkillsData(t: (key: string) => string) {
+  return [
+    {
+      category: t('category.languages'),
+      skills: [
+        { name: 'Python', icon: FaPython },
+        { name: 'Java', icon: FaJava },
+        { name: 'C', icon: FaC },
+        { name: 'PHP', icon: SiPhp },
+        { name: 'Go', icon: FaGolang },
+        { name: 'Kotlin', icon: SiKotlin },
+        { name: 'Javascript', icon: DiJavascript1 },
+        { name: 'Typescript', icon: SiTypescript },
+        { name: 'HTML', icon: FaHtml5 },
+        { name: 'CSS', icon: FaCss3Alt },
+      ],
+    },
+    {
+      category: t('category.frameworks'),
+      skills: [
+        { name: 'Django', icon: SiDjango },
+        { name: 'Spring', icon: SiSpring },
+        { name: 'React', icon: FaReact },
+        { name: 'Angular', icon: FaAngular },
+        { name: 'Vue.js', icon: FaVuejs },
+        { name: 'Node.js', icon: FaNodeJs },
+      ],
+    },
+    {
+      category: t('category.databases'),
+      skills: [
+        { name: 'SQL', icon: FaDatabase },
+        { name: 'PostgreSQL', icon: BiLogoPostgresql },
+        { name: 'MySQL', icon: GrMysql },
+        { name: 'NoSQL', icon: FaDatabase },
+        { name: 'MongoDB', icon: SiMongodb },
+        { name: 'Redis', icon: DiRedis },
+      ],
+    },
+    {
+      category: t('category.tools'),
+      skills: [
+        { name: 'Git', icon: FaGitAlt },
+        { name: 'CI/CD', icon: SiGithubactions },
+        { name: 'Docker', icon: FaDocker },
+        { name: 'Visual Studio Code', icon: BiLogoVisualStudio },
+        { name: 'Eclipse', icon: DiEclipse },
+        { name: 'IntelliJ', icon: SiIntellijidea },
+      ],
+    },
+  ];
+}
--- src/sections/SkillsSection.tsx
+++ src/sections/SkillsSection.tsx
@@ -1,87 +1,12 @@
 import { SkillsCategory } from '../components/SkillsCategory';
+import { getSkillsData } from './SkillsData';
 import { useLanguage } from '../hooks/useLanguage';
-import {
-  FaGitAlt,
-  FaPython,
-  FaDocker,
-  FaJava,
-  FaHtml5,
-  FaCss3Alt,
-  FaReact,
-  FaAngular,
-  FaVuejs,
-  FaNodeJs,
-  FaDatabase,
-} from 'react-icons/fa';
-import { FaC } from 'react-icons/fa6';
-import { FaGolang } from 'react-icons/fa6';
-import {
-  SiGithubactions,
-  SiPhp,
-  SiIntellijidea,
-  SiKotlin,
-  SiTypescript,
-  SiDjango,
-  SiSpring,
-  SiMongodb,
-} from 'react-icons/si';
-import { BiLogoVisualStudio } from 'react-icons/bi';
-import { DiEclipse, DiJavascript1, DiRedis } from 'react-icons/di';
-import { GrMysql } from 'react-icons/gr';
-import { BiLogoPostgresql } from 'react-icons/bi';
+import { useMemo } from 'react';
 
 function SkillsSection() {
   const { t } = useLanguage();
-  const skillsData = [
-    {
-      category: t('category.languages'),
-      skills: [
-        { name: 'Python', icon: FaPython },
-        { name: 'Java', icon: FaJava },
-        { name: 'C', icon: FaC },
-        { name: 'PHP', icon: SiPhp },
-        { name: 'Go', icon: FaGolang },
-        { name: 'Kotlin', icon: SiKotlin },
-        { name: 'Javascript', icon: DiJavascript1 },
-        { name: 'Typescript', icon: SiTypescript },
-        { name: 'HTML', icon: FaHtml5 },
-        { name: 'CSS', icon: FaCss3Alt },
-      ],
-    },
-    {
-      category: t('category.frameworks'),
-      skills: [
-        { name: 'Django', icon: SiDjango },
-        { name: 'Spring', icon: SiSpring },
-        { name: 'React', icon: FaReact },
-        { name: 'Angular', icon: FaAngular },
-        { name: 'Vue.js', icon: FaVuejs },
-        { name: 'Node.js', icon: FaNodeJs },
-      ],
-    },
-    {
-      category: t('category.databases'),
-      skills: [
-        { name: 'SQL', icon: FaDatabase },
-        { name: 'PostgreSQL', icon: BiLogoPostgresql },
-        { name: 'MySQL', icon: GrMysql },
-        { name: 'NoSQL', icon: FaDatabase },
-        { name: 'MongoDB', icon: SiMongodb },
-        { name: 'Redis', icon: DiRedis },
-      ],
-    },
-    {
-      category: t('category.tools'),
-      skills: [
-        { name: 'Git', icon: FaGitAlt },
-        { name: 'CI/CD', icon: SiGithubactions },
-        { name: 'Docker', icon: FaDocker },
-        { name: 'Visual Studio Code', icon: BiLogoVisualStudio },
-        { name: 'Eclipse', icon: DiEclipse },
-        { name: 'IntelliJ', icon: SiIntellijidea },
-      ],
-    },
-  ];
+
+  const skillsData = useMemo(() => getSkillsData(t), [t]);
 
   return (
     <ul className="max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-6">
